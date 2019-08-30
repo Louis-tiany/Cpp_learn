275f46ff98279b9b33a8566ef29a34c1fe82b141
Louis_tiany <tianyong.my@outlook.com>
2019-08-30 14:16:47 +0800

    add data_struct and complete disjont set algorithm

--- algorithm/greedy/jobSeq_set.cpp
+++ algorithm/greedy/jobSeq_set.cpp
@@ -1,5 +1,6 @@
+#include<limits.h>
+#include<algorithm>
 #include<iostream>
-#include<set>
 
 
 using namespace std;
@@ -17,7 +18,7 @@ struct disjointSet
 	disjointSet(int n)
 	{
 		parent = new int[n+1];
-		for(int i=0;i<n;i++)
+		for(int i=0;i<=n;i++)
 			parent[i]=i;
 	}
 	int find(int s)
@@ -28,25 +29,51 @@ struct disjointSet
 	}
 	void merge(int u,int v)
 	{
-		parent[c]=u;
+		parent[v]=u;
 	}
 };
 
-bool cmp(job a u,job b)
+bool cmp(struct job a,struct job b)
 {
 	return (a.profit>b.profit);
 }
 
-int findJobScheduling(job arr[],int n)
+int findMaxDeadline(struct job arr[],int n)
+{
+	int ans=-1;
+	for(int i=0;i<n;i++)
+		ans=max(ans,arr[i].deadline);
+	return ans;
+}
+
+int findJobScheduling(struct job arr[],int n)
 {
 	sort(arr,arr+n,cmp); 
-	maxDeadline=findMaxDeadline(arr,n);
+	int maxDeadline=findMaxDeadline(arr,n);
+	disjointSet ds(maxDeadline);
 
+	for(int i=0;i<n;i++)
+	{
+		int availableSlot=ds.find(arr[i].deadline);
+		if(availableSlot>0)
+		{
+			ds.merge(ds.find(availableSlot-1),availableSlot);
+			cout<<arr[i].id<<"    ";
+		}
+	}
+	return 0;
 }
 
 
 int main()
 {
 
+	struct job arr[] =  { { 'a', 2, 100 }, { 'b', 1, 19 },  
+                   { 'c', 2, 27 },  { 'd', 1, 25 },  
+                   { 'e', 3, 15 } }; 
+	int n=sizeof(arr)/sizeof(arr[0]);
+	cout<<"job be selected:"<<"\n";
+	findJobScheduling(arr,n);
+	cout<<endl;
 	return 0;
 }
